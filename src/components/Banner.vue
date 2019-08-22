253d27c7d03bd7183de959cd20def7c5088a6412
Rodolfo Jose Bagay (WSD) <rodolfo.b@tc-gaming.co>
2019-08-22 20:41:09 +0800

    anim

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,11 +2,11 @@
   <Container cls="flex v-center">
     <div class="b-con">
       <div class="b-text">
-        <h5 class="fw-lg">Delivering</h5>
-        <h5 class="fw-lg c-main">Competative</h5>
+        <h5 class="fw-lg scroll-fade-opacity">Delivering</h5>
+        <h5 class="fw-lg c-main scroll-fade-opacity fade-5">Competative</h5>
         <div class="b-ln">
-          <h5 class="fw-lg c-ther">Custom&nbsp;</h5>
-          <h5 class="fw-lg">Web Pages</h5>
+          <h5 class="fw-lg c-ther scroll-fade-opacity fade-8">Custom&nbsp;</h5>
+          <h5 class="fw-lg scroll-fade-opacity">Web Pages</h5>
         </div>
         <p>Integer ornare varius hendrerit. Nullam enim augue, rutrum sed elit sed, pharetra fermentum neque. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
         <VButton
@@ -45,6 +45,7 @@ export default {
   flex-direction: column;
   flex: 1 0 49%;
   margin-right:2%;
+  overflow: hidden;
 }
 .b-img {
   flex: 1 0 49%
